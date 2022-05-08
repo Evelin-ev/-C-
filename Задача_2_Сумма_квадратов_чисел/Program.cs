091e5aad165e384330d4e3107dcd235a60335a28
Evelina <evelina.evish@yandex.ru>
2022-05-08 10:35:58 +0300

    Добавила Задача_2_Сумма_квадратов_чисел. Программа работает

---
+++ Задача_2_Сумма_квадратов_чисел/Program.cs
@@ -0,0 +1,20 @@
+﻿// Сумма квадратов чисел
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int result1 = number1 * number1;
+Console.WriteLine("Квадрат " + number1 + " = " + result1);
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+int result2 = number2 * number2;
+Console.WriteLine("Квадрат " + number2 + " = " + result2);
+
+int sum = result1 + result2;
+
+Console.WriteLine("       ");
+Console.WriteLine("Квадрат " + number1 + " + квадрат " + number2 + " = " + sum);
+
