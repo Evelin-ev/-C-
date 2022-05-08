817e00a4434349cc5545cfecf5cf9116ab593b2f
Evelina <evelina.evish@yandex.ru>
2022-05-08 09:46:00 +0300

    Добавила Задача_1_Квадрат_числа. Приложение работает

---
+++ Задача_1_Квадрат_числа/Program.cs
@@ -0,0 +1,6 @@
+﻿// Программа на входе принимающая число, а на выходе выдающая его квадрат
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine(number + " в квадрате = " + result);
