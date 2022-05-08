d5f6c4f0d7b8486214a695a3e359bbba301d2fab
Evelina <evelina.evish@yandex.ru>
2022-05-08 09:52:44 +0300

    Добавила Задача_1_Квадрат_числа_рандом. Приложение работает

---
+++ Задача_1_Квадрат_числа_рандом/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа на входе принимающая рандомное число, а на выходе выдающая его квадрат
+
+
+int number = new Random().Next(1, 1000000);
+int result = number * number;
+
+Console.WriteLine(number + " в квадрате = " + result);
